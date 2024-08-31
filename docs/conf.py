8a0faea21cd3fe00aed9eb4768da5bcd8a25a8dd
l <56237933+lcdunne@users.noreply.github.com>
2024-08-31 21:53:33 +0100

    add theme opts

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,15 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "alabaster"
+html_theme_options = {
+    "github_user": "lcdunne",
+    "github_repo": "flask-reqcheck",
+    "github_banner": True,
+    "github_button": True,
+    "github_type": "star",
+    "fixed_sidebar": True,
+}
+
 html_static_path = ["_static"]
 
 # -- Options for todo extension ----------------------------------------------
